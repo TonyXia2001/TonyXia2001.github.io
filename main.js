33560dfbe7c4b0fca0565e7b5385bdae4dfbe7aa
Tony Xia <summertony717@gmail.com>
2019-12-20 13:34:18 -0500

    made some minor fixes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var learnMoreButton = document.getElementById("learnMoreButton");
 learnMoreButton.addEventListener("click", (e)=>{
     learnMoreButton.style.display = "none";
+    // document.getElementById("about").style.height = "2em";
     document.getElementById("about").classList.add("graduallyShown");
 }, false);
